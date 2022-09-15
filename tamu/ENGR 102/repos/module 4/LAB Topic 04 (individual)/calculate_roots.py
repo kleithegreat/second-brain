c6d5665b13fe9e768a325f40ee0f27fee08e0d2c
Kevin Lei <kevvinlei89@gmail.com>
2022-09-14 23:15:29 -0500

    update

--- tamu/ENGR 102/repos/module 4/LAB Topic 04 (individual)/calculate_roots.py
+++ tamu/ENGR 102/repos/module 4/LAB Topic 04 (individual)/calculate_roots.py
@@ -6,3 +6,14 @@
 # Section:      522
 # Assignment:   
 # Date:         
+
+a = float(input("Please enter the coefficient A: "))
+b = float(input("Please enter the coefficient B: "))
+c = float(input("Please enter the coefficient C: "))
+
+if a == b == c == 0:
+    print("The root is x = 0")
+if (a and b == 0) and (c != 0):
+    print("You entered an invalid combination of coefficients!")
+elif a == 0:
+    print(f"The root is x = {(-1 * c) / b}")
