a810b45610ab1c1c8a5a1cbc1aef51a099abce5a
Kevin Lei <kevvinlei89@gmail.com>
2022-09-20 22:43:54 -0500

    update

---
+++ tamu/ENGR 102/repos/module 5/LAB Topic 05 (individual)/boiling_curve.py
@@ -0,0 +1,9 @@
+# By submitting this assignment, I agree to the following:
+#   "Aggies do not lie, cheat, or steal, or tolerate those who do."
+#   "I have not given or received any unauthorized aid on this assignment."
+#
+# Name:         Kevin Lei
+# Section:      522
+# Assignment:   5.4.1: LAB: Boiling curve
+# Date:         23 September 2022
+
