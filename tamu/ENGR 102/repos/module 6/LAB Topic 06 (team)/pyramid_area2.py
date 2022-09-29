cc68af5c0d9020e5519d2a6f83ca759bc127566c
Kevin Lei <kevvinlei89@gmail.com>
2022-09-29 18:14:42 -0500

    update

---
+++ tamu/ENGR 102/repos/module 6/LAB Topic 06 (team)/pyramid_area2.py
@@ -0,0 +1,19 @@
+# By submitting this assignment, I agree to the following:
+#   "Aggies do not lie, cheat, or steal, or tolerate those who do."
+#   "I have not given or received any unauthorized aid on this assignment."
+#
+# Names:        Kevin Lei
+#               Bryan Sanchez Chavez
+#               Euijin Kim
+#               Agustin Portillo
+# Section:      522
+# Assignment:   6.12.1: LAB: Pyramid area (part 2)
+# Date:         30 September 2022
+
+from math import sqrt
+
+length = float(input("Enter the side length in meters: "))
+layers = int(input("Enter the number of layers: "))
+area = (layers / 2) * (2 *  + (layers - 1))
+
+print(f"You need {area:.2f} m^2 of gold foil to cover the pyramid")
