2e953e53743624b08531278344b59d0400ed985c
Kevin Lei <kevvinlei89@gmail.com>
2022-10-04 19:32:52 -0500

    update

--- tamu/ENGR 102/repos/module 7/LAB Topic 07 (individual)/kaprekars_challenge.py
+++ tamu/ENGR 102/repos/module 7/LAB Topic 07 (individual)/kaprekars_challenge.py
@@ -7,3 +7,45 @@
 # Assignment:   7.29.1: LAB: Kaprekar's constant challenge
 # Date:         07 October 2022
 
+count = 0
+ascending = [0, 0, 0, 0]
+def increment():
+    ascending[3] += 1
+    i = 0
+    while i < 4:
+        if ascending[i] == 10:
+            ascending[i - 1] += 1
+            ascending[i] = 0
+            i -= 1
+            continue
+        i += 1
+
+iteration = 0
+while iteration < 10000:
+    if ascending[0] == ascending[1] and ascending[1] == ascending[2] and ascending[2] == ascending[3]:
+        increment()
+        continue
+
+    while True:
+        ascending.sort()
+        descending = [x for x in reversed(ascending)]
+
+        for i in range(4):
+            ascending[i] = descending[i] - ascending[i]
+        count += 1
+        i = 0
+        while i < len(ascending):
+            if ascending[i] < 0:
+                ascending[i - 1] -= 1
+                ascending[i] += 10
+                i -= 1
+                continue
+            i += 1
+
+        if ascending == [6, 1, 7, 4]:
+            break
+    
+    increment()
+    iteration += 1
+
+print(f"Kaprekar's routine takes {count} total iterations for all four-digit numbers")
--- tamu/ENGR 102/repos/module 7/LAB Topic 07 (individual)/kaprekars_constant.py
+++ tamu/ENGR 102/repos/module 7/LAB Topic 07 (individual)/kaprekars_constant.py
@@ -7,3 +7,30 @@
 # Assignment:   7.28.1: LAB: Kaprekar's constant
 # Date:         07 October 2022
 
+userInput = input("Enter a four-digit integer: ")
+ascending = [int(x) for x in userInput]
+count = 0
+print(userInput, end = "")
+
+while True:
+    ascending.sort()
+    descending = [x for x in reversed(ascending)]
+
+    for i in range(4):
+        ascending[i] = descending[i] - ascending[i]
+    count += 1
+    i = 0
+    while i < len(ascending):
+        if ascending[i] < 0:
+            ascending[i - 1] -= 1
+            ascending[i] += 10
+            i -= 1
+            continue
+        i += 1
+
+    print(f" > {int(''.join(map(str,ascending)))}", end = "")
+    if ascending == [6, 1, 7, 4]:
+        break
+
+print()
+print(f"{userInput} reaches 6174 via Kaprekar's routine in {count} iterations")
