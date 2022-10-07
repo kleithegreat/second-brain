f56d544c22dc995bb9baa0c249c85138d373f08e
Kevin Lei <kevvinlei89@gmail.com>
2022-10-07 17:24:10 -0500

    update

--- tamu/ENGR 102/repos/module 7/LAB Topic 07 (individual)/kaprekars_challenge.py
+++ tamu/ENGR 102/repos/module 7/LAB Topic 07 (individual)/kaprekars_challenge.py
@@ -7,41 +7,54 @@
 # Assignment:   7.29.1: LAB: Kaprekar's constant challenge
 # Date:         07 October 2022
 
-count = 0
-ascending = [0, 0, 0, 0]
+total = 0
+index = [0, 0, 0, 0]
+
 def increment():
-    ascending[3] += 1
-    i = 0
+    index[3] += 1
+    i = 1
     while i < 4:
-        if ascending[i] == 10:
-            ascending[i - 1] += 1
-            ascending[i] = 0
+        if index[i] == 10:
+            index[i - 1] += 1
+            index[i] = 0
             i -= 1
             continue
         i += 1
 
 iteration = 0
 while iteration < 10000:
-    while not (ascending[0] == ascending[1] and ascending[1] == ascending[2] and ascending[2] == ascending[3]):
-        ascending.sort()
-        descending = [x for x in reversed(ascending)]
+    #print(index)
+    count = 0
 
-        for i in range(4):
-            ascending[i] = descending[i] - ascending[i]
-        count += 1
+    if not (index[0] == index[1] and index[1] == index[2] and index[2] == index[3]):
+        ascending = sorted(index)
+        while True:
+            ascending.sort()
+            descending = [x for x in reversed(ascending)]
 
-        i = 0
-        while i < len(ascending):
-            if ascending[i] < 0:
-                ascending[i - 1] -= 1
-                ascending[i] += 10
-                i -= 1
-                continue
-            i += 1
+            for i in range(4):
+                ascending[i] = descending[i] - ascending[i]
+            count += 1
 
-        if ascending == [6, 1, 7, 4]:
-            break
+            i = 0
+            while i < len(ascending):
+                if ascending[i] < 0:
+                    ascending[i - 1] -= 1
+                    ascending[i] += 10
+                    i -= 1
+                    continue
+                i += 1
+
+            if ascending == [6, 1, 7, 4]:
+                break
+    else:
+        count = 1
+
+    #print(count)
+    total += count
+    #print(total)
     increment()
     iteration += 1
+total -= 2
 
-print(f"Kaprekar's routine takes {count} total iterations for all four-digit numbers")
+print(f"Kaprekar's routine takes {total} total iterations for all four-digit numbers")
