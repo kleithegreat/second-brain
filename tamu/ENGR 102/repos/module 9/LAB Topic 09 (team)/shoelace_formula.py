b1ccecb1ae1f69867b267c07fec969da296b7dc0
Kevin Lei <kevvinlei89@gmail.com>
2022-10-26 13:50:50 -0500

    update

--- tamu/ENGR 102/repos/module 9/LAB Topic 09 (team)/shoelace_formula.py
+++ tamu/ENGR 102/repos/module 9/LAB Topic 09 (team)/shoelace_formula.py
@@ -12,29 +12,27 @@
 # Date:         24 October 2022
 
 def getpoints(stringIn):
-    points = []
-    for i in range(len(stringIn)):
-        try:
-            if stringIn[i] in "0123456789" and stringIn[i + 1] == "," and stringIn[i + 2] in "0123456789":
-                points += [int(stringIn[i]), int(stringIn[i + 2])]
-        except:
-            continue
-    return points
-    
+    pointsList = stringIn.split(" ")
+    for i in pointsList:
+        pointsList[pointsList.index(i)] = i.split(",")
+    for i in pointsList:
+        for j in i:
+            pointsList[pointsList.index(i)][i.index(j)] = int(j)
+    return pointsList
     
 def cross(pointA, pointB):
     return pointA[0] * pointB[1] - pointA[1] * pointB[0]
     
 def shoelace(points):
     crossTotal = 0
-    for i in range(len(getpoints(points))):
-        crossTotal += cross(getpoints(points)[i], getpoints(points)[i + 2])
+    for i in range(len(points) - 1):
+        crossTotal += cross(points[i], points[i + 1])
+    crossTotal += cross(points[len(points) - 1], points[0])
     return crossTotal / 2
-    
+
 def main():
     points = input("Please enter the verticies: ")
-    getpoints(points)
-    print(f"The area of the polygon is {shoelace(points)}")
+    print(f"The area of the polygon is {shoelace(getpoints(points))}")
     
 if __name__ == "__main__":
     main()
