db2bd42045e39b7ca6bf62cb766691aea860e132
Kevin Lei <kevvinlei89@gmail.com>
2022-10-26 10:47:16 -0500

    update

--- tamu/ENGR 102/repos/module 9/LAB Topic 09 (individual)/small_functions.py
+++ tamu/ENGR 102/repos/module 9/LAB Topic 09 (individual)/small_functions.py
@@ -10,5 +10,16 @@
 def parta(rSphere: float, rHole: float):
     return (4 / 3) * (rSphere ** 2 - rHole ** 2) ** (3 / 2)
 
-def partb(n):
-    
+def partb(n: int):
+    for i in range(1, n, 2):
+        ints =[]
+        for j in range(i, n, 2):
+            ints.append(j)
+            if sum(ints) == n:
+                return ints
+            elif sum(ints) > n:
+                break
+    return False
+
+def partc(char: str, name: str, company: str, email: str):
+    longest = max(len(name), len(company), len(email))
