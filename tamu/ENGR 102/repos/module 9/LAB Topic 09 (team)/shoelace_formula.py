90f89d7696a925c7265a6615da3e21e0e31d2121
Kevin Lei <kevvinlei89@gmail.com>
2022-10-28 13:00:47 -0500

    update

--- tamu/ENGR 102/repos/module 9/LAB Topic 09 (team)/shoelace_formula.py
+++ tamu/ENGR 102/repos/module 9/LAB Topic 09 (team)/shoelace_formula.py
@@ -11,7 +11,7 @@
 # Assignment:   9.15.1: LAB: Shoelace formula
 # Date:         24 October 2022
 
-def getpoints(stringIn):
+def getpoints(stringIn): # takes a string of points and turns it into a list for computation
     pointsList = stringIn.split(" ")
     for i in pointsList:
         pointsList[pointsList.index(i)] = i.split(",")
@@ -19,11 +19,11 @@ def getpoints(stringIn):
         for j in i:
             pointsList[pointsList.index(i)][i.index(j)] = int(j)
     return pointsList
-    
-def cross(pointA, pointB):
+
+def cross(pointA, pointB): # returns the cross product of two given points
     return pointA[0] * pointB[1] - pointA[1] * pointB[0]
     
-def shoelace(points):
+def shoelace(points): # returns the area of a polygon given an array of points using the shoelace formula
     crossTotal = 0
     for i in range(len(points) - 1):
         crossTotal += cross(points[i], points[i + 1])
@@ -31,7 +31,7 @@ def shoelace(points):
     return crossTotal / 2
 
 def main():
-    points = input("Please enter the verticies: ")
+    points = input("Please enter the vertices: ")
     print(f"The area of the polygon is {shoelace(getpoints(points))}")
     
 if __name__ == "__main__":
