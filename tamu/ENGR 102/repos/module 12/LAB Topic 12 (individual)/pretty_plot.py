321d7f3450b7d893baad8679d12b7240ac8063a6
Kevin Lei <kevvinlei89@gmail.com>
2022-11-12 19:00:04 -0600

    update

---
+++ tamu/ENGR 102/repos/module 12/LAB Topic 12 (individual)/pretty_plot.py
@@ -0,0 +1,10 @@
+# By submitting this assignment, I agree to the following:
+#   "Aggies do not lie, cheat, or steal, or tolerate those who do."
+#   "I have not given or received any unauthorized aid on this assignment."
+#
+# Name:         Kevin Lei
+# Section:      522
+# Assignment:   12.16.1: LAB: Pretty plot
+# Date:         14 November 2022
+
+import numpy as np
---
+++ tamu/ENGR 102/repos/module 12/LAB Topic 12 (team)/matplotlib_example.py
@@ -0,0 +1,13 @@
+# By submitting this assignment, I agree to the following:
+#   "Aggies do not lie, cheat, or steal, or tolerate those who do."
+#   "I have not given or received any unauthorized aid on this assignment."
+#
+# Names:        Kevin Lei
+#               Quinn Hamilton
+#               Jinyu Zhou
+#               Joaquin Torres
+#               
+# Section:      522
+# Assignment:   12.15.1: LAB: Matplotlib example
+# Date:         14 November 2022
+
