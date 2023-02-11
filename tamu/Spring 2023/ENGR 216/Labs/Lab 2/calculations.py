fe2c7f6bea54e3c7ce909545b20e31d1ff67f72b
Kevin Lei <kevvinlei89@gmail.com>
2023-02-10 19:49:07 -0500

    .

--- tamu/Spring 2023/ENGR 216/Labs/Lab 2/calculations.py
+++ tamu/Spring 2023/ENGR 216/Labs/Lab 2/calculations.py
@@ -1,1 +1,19 @@
 import pandas as pd
+
+rulerData = pd.read_csv("ruler.csv")
+puckData = pd.read_csv("puck.csv")
+
+# removing unused columns from both dataframes
+for col_name in rulerData.columns:
+    if col_name not in ["frame_no", "timestamp", "size_px-hotpink", "position_px_x-hotpink", "position_px_y-hotpink", "size_px-lightorange", "position_px_x-lightorange", "position_px_y-lightorange"]:
+        del rulerData[col_name]
+
+for col_name in puckData.columns:
+    if col_name not in ["frame_no", "timestamp", "size_px-hotpink", "position_px_x-hotpink", "position_px_y-hotpink"]:
+        del puckData[col_name]
+
+#print(rulerData.describe())
+#print(puckData.describe())
+
+# array of lengths of the ruler for each frame collected
+rulerLengths = []
