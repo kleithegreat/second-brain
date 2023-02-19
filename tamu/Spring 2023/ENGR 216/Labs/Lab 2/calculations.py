3c7b8dd3b794bb2f76e886ae4030dffbdc948be2
Kevin Lei <kevvinlei89@gmail.com>
2023-02-19 17:26:37 -0600

    .

--- tamu/Spring 2023/ENGR 216/Labs/Lab 2/calculations.py
+++ tamu/Spring 2023/ENGR 216/Labs/Lab 2/calculations.py
@@ -1,4 +1,4 @@
-from math import sqrt
+from math import sqrt, sin, pi
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -13,48 +13,50 @@ for col_name in rulerData.columns:
         del rulerData[col_name]
 
 for col_name in puckData.columns:
-    if col_name not in ["frame_no", "timestamp", "position_px_x-hotpink", "position_px_y-hotpink"]:
+    if col_name not in ["frame_no", "timestamp", "position_px_y-hotpink"]:
         del puckData[col_name]
 
-# drop all empty rows
-rulerData.dropna(subset=["position_px_x-hotpink"], inplace=True)
-puckData.dropna(subset=["position_px_y-hotpink"], inplace=True)
+puckData.dropna(subset=["position_px_y-hotpink"], inplace=True) # drops all empty rows
+
+
 
 ### RULER STUFF
-
-# array of lengths of the ruler for each frame collected
 def distance(row):
     return sqrt( (row["position_px_x-hotpink"] - row["position_px_x-lightorange"])**2 + (row["position_px_y-hotpink"] - row["position_px_y-lightorange"])**2 )
-
 rulerData["length"] = rulerData.apply(distance, axis=1)
 averageLength = rulerData["length"].mean()
+
 print(f"Average ruler length in pixels: {averageLength}")
 
-# to meters
+# conversion factor
 metersPerPixel = 0.5 / averageLength
 
+# error propagation TODO: FIGURE THIS SHIT OUT
+
+
 ### PUCK STUFF
+
 startTime = 9600
 endTime = 10900
 puckData = puckData[(puckData['timestamp'] >= startTime) & (puckData['timestamp'] <= endTime)]
 
 puckData["time_s"] = puckData["timestamp"] / 1000
 
-puckData.to_csv("puckDataProcessed.csv", index=False)
-
 # moving averages and finite differences
-period = 16
+period = 3
 puckData["yposavg"] = puckData["position_px_y-hotpink"].rolling(window=period).mean()
 
 puckData["yvelocity"] = puckData["yposavg"].diff() * metersPerPixel / puckData["time_s"].diff()
 
-period = 10
+period = 4
 puckData["yvelavg"] = puckData["yvelocity"].rolling(window=period).mean()
 
 puckData["yaccel"] = puckData["yvelavg"].diff() / puckData["time_s"].diff()
 
-period = 5
+period = 3
 puckData["yaccelavg"] = puckData["yaccel"].rolling(window=period).mean()
+
+puckData.to_csv("puckDataProcessed.csv", index=False)
 
 # plotting position vs time
 
@@ -78,6 +80,14 @@ plt.plot(puckData["time_s"], puckData["yaccel"])
 plt.title("ay vs t")
 
 plt.show()
+
+# average acceleration
+a = puckData["yaccelavg"].mean()
+print(f"Average acceleration: {a}")
+
+# finding g
+g = puckData["yaccelavg"].mean() / sin(3.6 * pi / 180)
+print(f"Average g: {g}")
 
 ### DEBUGGING
 #print(rulerData.describe())
