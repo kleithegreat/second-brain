64aa58368577213954c34899d55a21e5fdd878cc
kevin <kevvinlei89@gmail.com>
2023-09-03 12:38:21 -0500

    lmao

---
+++ tamu/2 fall 2023/CSCE 120/Homework/HW_1/mountains_valleys.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "functions.h"
+
+using std::cout, std::cin, std::endl;
+
+int main() {
+	// TODO(student): print prompt for input
+	
+	// TODO(student): read the numbers from standard input
+
+	// TODO(student): validate input (and reprompt on invalid input)
+
+	// TODO(student): compute and display solution
+	
+	return 0;
+}
