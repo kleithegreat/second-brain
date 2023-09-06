840ea4f3161e1f743e2306c680d9963164ec0d54
kevin <kevvinlei89@gmail.com>
2023-09-06 18:27:20 -0500

    lmao

--- tamu/2F23/CSCE_120/homework/hw2/functions.cpp
+++ tamu/2F23/CSCE_120/homework/hw2/functions.cpp
@@ -4,18 +4,80 @@
 using std::cout, std::endl;
 
 bool is_valid_range(int a, int b) {
-	// TODO(student): validate input range
-	return false;
+    if (a >= 10 && a <= b && b < 10000) {
+        return true;
+    }
+    return false;
 }
 
 char classify_mv_range_type(int number) {
-	// TODO(student): return 'M' if number has /\/\... pattern,
-	// return 'V' if number has \/\/ pattern, and return 'N' otherwise
-	return 'N';
+    if (number < 10) {
+        return 'N';
+    }
+
+    int numDigits = 0;
+    int temp = number;
+    while (temp > 0) {
+        numDigits++;
+        temp /= 10;
+    }
+
+    int divisor = 1;
+    for (int i = 1; i < numDigits; i++) {
+        divisor *= 10;
+    }
+
+    int lastDigit = number / divisor;
+    number %= divisor;
+    divisor /= 10;
+
+    int currentDigit;
+    char initialPattern = '0';
+    char nextPattern;
+
+    while (divisor > 0) {
+        currentDigit = number / divisor;
+        number %= divisor;
+        divisor /= 10;
+
+        if (initialPattern == '0') {
+            if (currentDigit > lastDigit) {
+                initialPattern = 'U';
+                nextPattern = 'D';	 
+            } else if (currentDigit < lastDigit) {
+                initialPattern = 'D';
+                nextPattern = 'U';
+            } else {
+                return 'N';
+            }
+        } else {
+            if (nextPattern == 'U' && !(currentDigit > lastDigit)) {
+                return 'N';  
+            } else if (nextPattern == 'D' && !(currentDigit < lastDigit)) {
+                return 'N';  
+            }
+            nextPattern = (nextPattern == 'U') ? 'D' : 'U';
+        }
+        lastDigit = currentDigit;
+    }
+
+    if (initialPattern == 'U') {
+        return 'M';
+    } else {
+        return 'V';
+    }
 }
 
 void count_valid_mv_numbers(int a, int b) {
-	// TODO(student): count the number of valid mountain ranges and valley
-	// ranges in the range [a, b] and print out to console using the format
-	// in Requirement 4 of the homework prompt
+    int m = 0;
+    int v = 0;
+    for (int i = a; i <= b; i++) {
+        if (classify_mv_range_type(i) == 'M') {
+            m++;
+        }
+        else if (classify_mv_range_type(i) == 'V') {
+            v++;
+        }
+    }
+    cout << "There are " << m << " mountain ranges and " << v << " valley ranges between " << a << " and " << b << "." << endl;
 }
--- tamu/2F23/CSCE_120/homework/hw2/mountains_valleys.cpp
+++ tamu/2F23/CSCE_120/homework/hw2/mountains_valleys.cpp
@@ -5,12 +5,26 @@ using std::cout, std::cin, std::endl;
 
 int main() {
 	// TODO(student): print prompt for input
+	cout << "Enter numbers 10 <= a <= b < 10000: ";
 	
 	// TODO(student): read the numbers from standard input
+	int a;
+	int b;
+	cin >> a;
+	cin >> b;
 
 	// TODO(student): validate input (and reprompt on invalid input)
+	while (!is_valid_range(a, b)) {
+		cout << "Invalid Input" << endl;
+		cout << "Enter numbers 10 <= a <= b < 10000: ";
+		cin >> a;
+		cin >> b;
+	}
 
 	// TODO(student): compute and display solution
+	count_valid_mv_numbers(a, b);
 	
+	//cout << classify_mv_range_type(1503) << endl;
+
 	return 0;
 }
