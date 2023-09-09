16850362bd0617de1dda59bef5c878791c2502c7
kevin <kevvinlei89@gmail.com>
2023-09-09 16:16:43 -0500

    lmao

---
+++ tamu/2F23/CSCE_120/homework/hw3/main.cpp
@@ -0,0 +1,51 @@
+/******************************************************************************
+
+File: main.cpp
+Author: Prof. Lupoli & Andrew Nemec
+Date: 2/16/2021
+Section: 
+E-mail: slupoli@tamu.edu
+Description:
+This file contains C++  code that will simulate data collected from a track 
+event with 9 lanes. The code will sort the order, then display the results
+
+Updates
+Author: J. Michael Moore & Paul Taele
+Author: J. Michael Moore
+
+Input information format
+<Time (double)> <Abbreviated Country (String)> <Uniform number (int)> <Lastname (String)>
+32.7 USA 12 Moore
+36.5 RUS 35 Polsley
+45.8 ENG 73 Teale
+52.34 CHN 14 Nemec
+76.45 ITY 23 Lupoli     
+
+*******************************************************************************/
+
+#include <iostream>
+#include <cstring>
+#include "parallel_tracks.h"
+
+using std::cin; using std::cout; using std::endl;
+
+int main()
+{
+    // TODO: create arrays needed
+	
+    // TOTO: prep all arrays
+	
+    // TODO: load the runner's data into the array
+
+    // if unable to load runner data
+    //   1) output "Bad input" to standard out
+    //   2) exit program by returning 1
+
+    // TODO: determine ranking, notice the rank array receives the results
+
+    // TODO: Output results
+
+    // this is not required in C++ but many people still explitly add it
+    return 0; 
+}
+
---
+++ tamu/2F23/CSCE_120/homework/hw3/parallel_tracks.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+#include <iomanip>
+#include <cstring>
+#include "parallel_tracks.h"
+
+using std::cin, std::cout, std::endl;
+
+//-------------------------------------------------------
+// Name: prep_double_array
+// PreCondition:  an array of doubles is passed in
+// PostCondition: data in the array is 'zeroed' out
+//---------------------------------------------------------
+void prep_double_array(double ary[])
+// making sure all values within the array are set to 0.0;
+{
+  //TODO
+}
+
+//-------------------------------------------------------
+// Name: prep_unsigned_int_array
+// PreCondition:  an array of unsigned ints is passed in
+// PostCondition: data in the array is 'zeroed' out
+//---------------------------------------------------------
+void prep_unsigned_int_array(unsigned int ary[])
+// making sure all values within the array are set to 0;
+{
+  //TODO
+}
+
+//-------------------------------------------------------
+// Name: prep_string_array
+// PreCondition:  an array of strings is passed in
+// PostCondition: each element in the array is set to "N/A"
+//---------------------------------------------------------
+void prep_string_array(char ary[][STRING_SIZE])
+// making sure all values within the array are set to "N/A";
+{
+	//TODO
+}
+
+//-------------------------------------------------------
+// Name: trim
+// PreCondition:  the cstring
+// PostCondition: whitespace has been removed from beginning and end of string
+//---------------------------------------------------------
+void trim(char str[STRING_SIZE]) {
+	// TODO
+}
+
+//-------------------------------------------------------
+// Name: get_runner_data
+// PreCondition:  the prepped parallel arrays
+// PostCondition: all arrays contain data from standard in
+//---------------------------------------------------------
+bool get_runner_data(double timeArray[], char countryArray[][STRING_SIZE], 
+		unsigned int numberArray[], char lastnameArray[][STRING_SIZE]) 
+{
+  //TODO
+  return true; // set so it will compile
+}
+
+//-------------------------------------------------------
+// Name: get_ranking
+// PreCondition:  just the time array is passed in, and has valid data
+// PostCondition: after a very inefficient nested loop to determine the placements 
+// and places the ranks in a new array. That new array is returned
+//---------------------------------------------------------
+void get_ranking(const double timeArray[], unsigned int rankArray[])
+{
+	//TODO
+}
+
+
+//-------------------------------------------------------
+// Name: print_results
+// PreCondition:  all parallel arrays are passed in and have valid data
+// PostCondition: after a very inefficient nested loop to determine the ranks
+// it then displays them along with a delta in time from the start
+//---------------------------------------------------------
+void print_results(const double timeArray[], const char countryArray[][STRING_SIZE],
+		const char lastnameArray[][STRING_SIZE], const unsigned int rankArray[])
+{
+
+	std::cout << "Final results!!";
+	std::cout << std::setprecision(2) << std::showpoint << std::fixed << std::endl;
+	double best_time = 0.0;
+		
+	// print the results, based on rank, but measure the time difference_type
+	for(unsigned int j = 1; j <= SIZE; j++)
+	{
+		
+		// go thru each array, find who places in "i" spot
+		for(unsigned int i = 0; i < SIZE; i++)
+		{
+			if(rankArray[i] == 1) // has to be a better way, but need the starting time
+			{
+				best_time = timeArray[i];
+			}
+			
+			
+			if(rankArray[i] == j) // then display this person's data
+			{
+				// this needs precision display
+				std::cout << "[" << j << "]  " << timeArray[i] << " " << std::setw(15) << std::left << lastnameArray[i] << "\t" << "(" << countryArray[i] << ")  +" << (timeArray[i] - best_time) << std::endl; 
+			}
+			
+		}
+	}	
+}
