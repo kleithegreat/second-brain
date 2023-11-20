1d38adee4183828b4925e5ac1c554a11f363a408
Kevin Lei <kevvinlei89@gmail.com>
2023-11-19 23:07:02 -0600

    lmao

--- tamu/2F23/CSCE_120/homework/hw11/TemperatureDatabase.cpp
+++ tamu/2F23/CSCE_120/homework/hw11/TemperatureDatabase.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <vector>
 
 using std::cout, std::endl, std::string, std::ofstream, std::ifstream;
 
@@ -63,5 +64,101 @@ void TemperatureDatabase::outputData(const string& filename) {
 }
 
 void TemperatureDatabase::performQuery(const string& filename) {
-	// TODO: implement this function
+	ifstream file(filename);
+	ofstream results("result.dat");
+
+	if (!file.is_open()) {
+		cout << "Error: Unable to open " << filename << endl;
+		exit(1);
+	}
+
+	string line;
+	while (getline(file, line)) {
+		std::istringstream iss(line);
+
+		string id;
+		string query;
+		int yearStart;
+		int yearEnd;
+
+		if (!(iss >> id >> query >> yearStart >> yearEnd)) {
+			cout << "Error: Other invalid query" << endl;
+			continue;
+		}
+
+		if (query != "AVG" && query != "MODE") {
+			cout << "Error: Unsupported query " << query << endl;
+			continue;
+		}
+
+		if (yearStart < 1800 || yearEnd > 2023 || yearStart > yearEnd) {
+			cout << "Error: Invalid range " << yearStart << "-" << yearEnd << endl;
+			continue;
+		}
+
+		string value;
+
+		if (query == "AVG") {
+			std::vector<double> total;
+			Node* current = records.getHead();
+
+			while (current != nullptr) {
+				total.push_back(current->data.temperature);
+			}
+
+			double sum = 0;
+			for (size_t i = 0; i < total.size(); i++) {
+				sum += total[i];
+			}
+
+			if (total.size() == 0) {
+				value = "unknown";
+			} else {
+				double average = sum / total.size();
+				value = std::to_string(average);
+			}
+		} else {
+			std::vector<int> frequencies(101);
+			Node* current = records.getHead();
+
+			while (current != nullptr) {
+				int rounded;
+
+				int integerPart = static_cast<int>(current->data.temperature);
+				double fractionalPart = current->data.temperature - integerPart;
+
+				if (current->data.temperature >= 0) {
+					rounded = (fractionalPart >= 0.5) ? (integerPart + 1) : integerPart;
+				} else {
+					rounded = (fractionalPart <= -0.5) ? (integerPart - 1) : integerPart;
+				}
+
+				int index = rounded + 50;
+				frequencies[index]++;
+
+				current = current->next;
+			}
+
+			int max = 0;
+			int maxIndex = 0;
+			for (size_t i = 0; i < frequencies.size(); i++) {
+				if (frequencies[i] > max) {
+					max = frequencies[i];
+					maxIndex = i;
+				}
+			}
+
+			if (max == 0) {
+				value = "unknown";
+			} else {
+				double mode = maxIndex - 50;
+				value = std::to_string(mode);
+			}
+		}
+
+		results << id << " " << yearStart << " " << yearEnd << " " << query << " " << value << endl;
+	}
+
+	file.close();
+	results.close();
 }
