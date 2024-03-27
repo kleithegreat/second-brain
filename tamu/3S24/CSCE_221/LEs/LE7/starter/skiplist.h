0cc122f4c5e3d900dc396111be09cedc728d3784
Kevin Lei <kevvinlei89@gmail.com>
2024-03-27 18:00:25 -0500

    lmao

---
+++ tamu/3S24/CSCE_221/LEs/LE7/starter/skiplist.h
@@ -0,0 +1,73 @@
+#ifndef SKIPLIST_H
+#define SKIPLIST_H
+
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+using namespace std;
+
+class Node {
+public:
+    int key;
+    std::vector<Node*> forward;
+
+    Node(int key, int level) : key(key), forward(level + 1, nullptr) {}
+};
+
+class SkipList {
+private: 
+    double P;
+    int MAXLVL;
+    Node* header;
+    int level;
+
+public:
+    SkipList(int max_lvl, double p);
+    ~SkipList();
+    int randomLevel();
+    void insertElement(int key);
+    void deleteElement(int search_key);
+    bool searchElement(int key);
+    void displayList();
+};
+
+
+SkipList::SkipList(int max_lvl, double p) : MAXLVL(max_lvl), P(p), level(0) {
+    header = new Node(-1, MAXLVL);
+}
+
+SkipList::~SkipList() {
+   
+} 
+
+int SkipList::randomLevel() {
+    return -1;
+}
+
+void SkipList::insertElement(int key) {
+    
+}
+
+void SkipList::deleteElement(int search_key) {
+    
+}
+
+bool SkipList::searchElement(int key) {
+    return false;
+}
+
+void SkipList::displayList() {
+    std::cout << "\n*****Skip List******" << std::endl;
+    Node* head = header;
+    for (int lvl = 0; lvl <= level; lvl++) {
+        std::cout << "Level " << lvl << ": ";
+        Node* node = head->forward[lvl];
+        while (node != nullptr) {
+            std::cout << node->key << " ";
+            node = node->forward[lvl];
+        }
+        std::cout << std::endl;
+    }
+}
+
+#endif
