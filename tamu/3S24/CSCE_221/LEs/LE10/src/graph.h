3b011e8d4d356cc4fa12189f40f8daf85556a9b4
kevin <kevvinlei89@gmail.com>
2024-04-21 17:03:33 -0500

    lmao

---
+++ tamu/3S24/CSCE_221/LEs/LE10/src/graph.h
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <climits>
+#include <queue>
+#include <unordered_set>
+using namespace std;
+
+class Graph {
+    private:
+        int n;
+        typedef pair<int, int> Edge;
+        vector<list<Edge>> v;
+        
+    public:
+        Graph(int size = 2) {
+            n = size;
+            v.resize(n);
+        }
+
+        void addEdge(int x, int y, int w) {
+  
+        }
+
+        void print() {
+            for(int i = 0; i < n; i++) {
+                cout << "Node " << i << " is connected to: ";
+                for (pair<int,int> j : v[i]) {
+                    if(j.first != -1){
+                        cout << j.first << " : "<< j.second<<", ";
+                    }
+                }
+                cout << endl;
+            }
+        }
+
+        vector<pair<int, int>> dijkstra(int startNode) {
+            vector<pair<int, int>> p(n);
+            return p;
+        }
+
+        string printShortestPath(int startNode, int endNode) {
+           return "  ";
+        }
+};
+
+
+
