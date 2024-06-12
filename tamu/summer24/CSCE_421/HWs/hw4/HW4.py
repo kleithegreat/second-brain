615044a9d8ddc5fa8a3034b73fddcda74bbfff00
kevin <kevvinlei89@gmail.com>
2024-06-12 16:39:32 -0500

    lmao

--- tamu/summer24/CSCE_421/HWs/hw4/HW4.py
+++ tamu/summer24/CSCE_421/HWs/hw4/HW4.py
@@ -20,7 +20,6 @@ def read_test_data(filename: str) -> pd.DataFrame:
     '''
     return pd.read_csv(filename)
 
-
 # Prepare your input data and labels
 def prepare_data(df_train: pd.DataFrame, df_test: pd.DataFrame) -> tuple:
     '''
@@ -29,16 +28,15 @@ def prepare_data(df_train: pd.DataFrame, df_test: pd.DataFrame) -> tuple:
         return tuple of numpy arrays(train_data, train_label, test_data, test_label).
         may use .dropna, 
     '''
-    dr_train = df_train.dropna()
-    dr_test = df_test.dropna()
-
-    x_train = dr_train['x'].values
-    y_train = dr_train['y'].values
-
-    x_test = dr_test['x'].values
-    y_test = dr_test['y'].values
-
-    return x_train, y_train, x_test, y_test
+    df_train = df_train.dropna()
+    df_test = df_test.dropna()
+    
+    train_X = df_train.iloc[:, :-1].values
+    train_y = df_train.iloc[:, -1].values.reshape(-1, 1)
+    test_X = df_test.iloc[:, :-1].values
+    test_y = df_test.iloc[:, -1].values.reshape(-1, 1)
+    
+    return train_X, train_y, test_X, test_y
 
 # Implement LinearRegression class
 class LinearRegression:   
@@ -54,50 +52,33 @@ class LinearRegression:
         self.N, self.n = X.shape     
         self.W = np.zeros((self.n, 1))          
         self.b = 0
-        ### YOUR CODE HERE
-
-        ### YOUR CODE HERE   
-        
-        # data
-        ### YOUR CODE HERE 
-        
-        # gradient descent learning                  
-        ### YOUR CODE HERE
-        ## GRADIENT UPDATE, UPDATE USING ALL TRAINING SET 
-        # predict on data and calculate gradients 
-        ### YOUR CODE HERE
-
-        ### YOUR CODE HERE  
-          
-        # update weights
-        ### YOUR CODE HERE
-        pass
-
-      
-    # output      
+                        
+        for _ in range(self.iterations):
+            y_pred = self.predict(X)
+            dW = (1/self.N) * np.dot(X.T, (Y - y_pred))
+            db = (1/self.N) * np.sum(Y - y_pred)
+            
+            self.W += self.learning_rate * dW
+            self.b -= self.learning_rate * db
+           
     def predict(self, X):
-        ### YOUR CODE HERE
-
-        ### YOUR CODE HERE 
-        pass
+        return np.dot(X, self.W) + self.b
 
 # Calculate and print the mean square error of your prediction
 def MSE(y_test, pred):
     '''
         return the mean square error corresponding to your prediction
     '''
-    return np.sum((y_test - pred)**2) / len(y_test)
-
-
+    return np.mean((y_test - pred)**2)
 
 if __name__ == "__main__":
    
     data_path_train   = "./train2.csv"
-    data_path_test    = ".test.csv"
+    data_path_test    = "./test.csv"
     df_train, df_test = read_train_data(data_path_train), read_test_data(data_path_test)
 
-
     train_X, train_y, test_X, test_y = prepare_data(df_train, df_test)
+
     r = LinearRegression(learning_rate=0.0001, epoches=10)
     r.fit(train_X, train_y)
 
@@ -110,13 +91,11 @@ if __name__ == "__main__":
     print(preds.shape)
     print(test_y.shape)
     # Calculate and print the mean square error of your prediction
-    mean_square_error = MSE(test_y, preds[:,1])
+    mean_square_error = MSE(test_y, preds)
     print(mean_square_error) # I added this
 
     # plot your prediction and labels, you can save the plot and add in the report
     plt.scatter(test_X,test_y, label='data')
     plt.plot(test_X, preds)
     plt.legend()
-    plt.show()
-
-    
+    plt.show()   
