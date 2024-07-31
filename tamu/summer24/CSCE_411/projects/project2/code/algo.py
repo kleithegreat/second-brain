97727be3d83fc4cb9e464354eaa7075f16bfe9fe
kevin <kevvinlei89@gmail.com>
2024-07-30 21:03:01 -0500

    lmao

--- tamu/summer24/CSCE_411/projects/project2/code/algo.py
+++ tamu/summer24/CSCE_411/projects/project2/code/algo.py
@@ -1,1 +1,17 @@
 import pickle
+from pprint import pprint
+
+with open('./tests/Examples_of_AdjLists_of_Trees', 'rb') as f:
+    adjlist = pickle.load(f)
+
+with open('./tests/Examples_of_k_values', 'rb') as f:
+    k_values = pickle.load(f)
+
+with open('./tests/Examples_of_labelling', 'rb') as f:
+    labelling = pickle.load(f)
+
+pprint(adjlist)
+print()
+pprint(k_values)
+print()
+pprint(labelling)
